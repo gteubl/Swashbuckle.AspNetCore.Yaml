7cc21cf1a2dc2f012e2bab3163fbf41070dbb614
Guilherme Teubl Ferreira <gteubl@gmail.com>
2019-10-03 09:21:56 +0200

    Initial Commit

---
+++ Swashbuckle.AspNetCore.Yaml/Application/SwaggerYamlMiddleware.cs
@@ -0,0 +1,88 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Template;
+using YamlDotNet.Serialization;
+
+namespace Swashbuckle.AspNetCore.Swagger.Yaml
+{
+    public class SwaggerYamlMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ISwaggerProvider _swaggerProvider;
+        private readonly ISerializer _swaggerSerializer;
+        private readonly SwaggerYamlOptions _options;
+        private readonly TemplateMatcher _requestMatcher;
+
+        public SwaggerYamlMiddleware(
+            RequestDelegate next,
+            ISwaggerProvider swaggerProvider,
+            SwaggerYamlOptions options)
+        {
+            _next = next;
+            _swaggerProvider = swaggerProvider;
+            _swaggerSerializer = SwaggerYamlSerializerFactory.Create(options);
+            _options = options;
+            _requestMatcher = new TemplateMatcher(TemplateParser.Parse(_options.SwaggerOptions.RouteTemplate), new RouteValueDictionary());
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (!RequestingSwaggerDocument(httpContext.Request, out string documentName))
+            {
+                await _next(httpContext);
+                return;
+            }
+
+            var basePath = string.IsNullOrEmpty(httpContext.Request.PathBase)
+                ? null
+                : httpContext.Request.PathBase.ToString();
+
+            var swagger = _swaggerProvider.GetSwagger(documentName, null, basePath);
+
+            // One last opportunity to modify the Swagger Document - this time with request context
+            foreach (var filter in _options.SwaggerOptions.PreSerializeFilters)
+            {
+                filter(swagger, httpContext.Request);
+            }
+
+            await RespondWithSwaggerYaml(httpContext.Response, swagger);
+        }
+
+        private bool RequestingSwaggerDocument(HttpRequest request, out string documentName)
+        {
+            documentName = null;
+            if (request.Method != "GET") return false;
+
+            var routeValues = new RouteValueDictionary();
+            if (!_requestMatcher.TryMatch(request.Path, routeValues) || !routeValues.ContainsKey("documentName")) return false;
+
+            documentName = routeValues["documentName"].ToString();
+            return true;
+        }
+
+        private async Task RespondWithSwaggerYaml(HttpResponse response, SwaggerDocument swagger)
+        {
+            response.StatusCode = 200;
+            response.ContentType = "text/yaml;charset=utf-8";
+
+            var jsonBuilder = new StringBuilder();
+            using (var writer = new StringWriter(jsonBuilder))
+            {
+                _swaggerSerializer.Serialize(writer, swagger);
+
+                string rawJson = jsonBuilder.ToString();
+                rawJson = rawJson.Replace("unique-items", "uniqueItems");
+                rawJson = rawJson.Replace("max-length", "maxLength");
+                rawJson = rawJson.Replace("min-length", "minLength");
+                rawJson = rawJson.Replace("read-only", "readOnly");
+                rawJson = rawJson.Replace("\r\n\r\n", "\r\n");
+
+                await response.WriteAsync(rawJson, new UTF8Encoding(false));
+            }
+        }
+
+    }
+}
